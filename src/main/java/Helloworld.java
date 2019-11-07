5f1256b2e08dcb44ab8c2ada95484fdab97b5323
lzl <2848133264@qq.com>
2019-11-07 12:49:44 +0800

    commit

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,18 @@
+
+public class Helloworld {
+    /*public  static void main(String [] args){
+        //sout : 直接输出语句
+        System.out.println();
+        //
+    }*/
+    //快加 psvm 主方法的首字母
+    public static void main(String[] args) {
+        System.out.println("Helloworld.main");
+        Helloworld test =new Helloworld();
+        test.test1();
+    }
+    public String test1(){
+        System.out.println("ok");
+        return "test ok";
+    }
+}
